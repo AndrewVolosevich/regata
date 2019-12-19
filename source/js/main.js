a822019e51ae919cfd9dbbd5575d7fcd14a20912
AndrewVolosevich <volosevich1989@gmail.com>
2019-12-19 06:01:59 +0300

    night

--- source/js/main.js
+++ source/js/main.js
@@ -6,3 +6,31 @@ burger_button.addEventListener("click", function (e) {
   burger_button.classList.toggle("page-header__toggle--active");
   nav_menu.classList.toggle("main-nav--active");
 });
+
+
+let block_1 = document.querySelector(".description");
+let block_2 = document.querySelector(".feature");
+let block_3 = document.querySelector(".price");
+
+let description = document.querySelector("#description");
+let feature = document.querySelector("#feature");
+let price = document.querySelector("#price");
+
+
+description.addEventListener("click", function (e) {
+  block_1.classList.add("active");
+  block_2.classList.remove("active");
+  block_3.classList.remove("active");
+});
+
+feature.addEventListener("click", function (e) {
+  block_2.classList.add("active");
+  block_1.classList.remove("active");
+  block_3.classList.remove("active");
+});
+
+price.addEventListener("click", function (e) {
+  block_3.classList.add("active");
+  block_2.classList.remove("active");
+  block_1.classList.remove("active");
+});
--- source/js/slick-slider.js
+++ source/js/slick-slider.js
@@ -15,5 +15,30 @@ $(document).ready(function(){
     autoplaySpeed: 4000,
     arrows:	true,
     dots: true,
+    responsive: [
+	    {
+	      breakpoint: 659,
+	      settings: {
+	        slidesToShow: 2,
+	      }
+	    }
+    ]
+  });
+
+  $('.card-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows:	true,
+    dots: true,
+/*     responsive: [
+	    {
+	      breakpoint: 659,
+	      settings: {
+	        slidesToShow: 2,
+	      }
+	    }
+    ] */
   });
 });
